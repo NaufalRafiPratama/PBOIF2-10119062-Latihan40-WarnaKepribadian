03d7237ce9e23cb2d16d7e4a2594baf691803460
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-11-08 00:18:08 +0700

    Add files via upload

---
+++ PBOIF2-10119062-Latihan40-WarnaKepribadian/src/pboif2/pkg10119062/latihan40/warnakepribadian/PBOIF210119062Latihan40WarnaKepribadian.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119062.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : program ini berisi program untuk menampilkan
+ * warna kepribadian
+ */
+public class PBOIF210119062Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        WarnaKepribadian kepribadian = new WarnaKepribadian();
+        kepribadian.inputWarna();
+        kepribadian.inputNama();
+        kepribadian.cekKepribadian();
+    }
+    
+}
